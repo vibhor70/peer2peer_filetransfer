6d72551a72119d26fb2971f9f7ec7c9e383eadb1
Jai Singhal <jaisinghal48@gmail.com>
2019-10-17 11:49:00 +0530

    Add indentation

--- server_con.c
+++ server_con.c
@@ -7,65 +7,65 @@
 #include <arpa/inet.h> 
 #include <netinet/in.h> 
 
-#define PORT	 8081 
+#define PORT	 8085
 #define MAXLINE  99999 
 
 int main(){
-size_t recvLen,newLen = 46258;
-char msg[MAXLINE];
-int sockfd;
-struct sockaddr_in serv_addr,cli_addr;
-if((sockfd = socket(PF_INET,SOCK_STREAM,0))<0){
-    perror("Server socket not created");
-} 
-memset (&serv_addr,0,sizeof(serv_addr));
-serv_addr.sin_family = AF_INET;
-serv_addr.sin_port = htons(PORT);
-serv_addr.sin_addr.s_addr =  htonl(INADDR_ANY);
+    size_t recvLen,newLen = 46258;
+    char msg[MAXLINE];
+    int sockfd;
+    FILE *fp1;
 
-if(bind(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr))<0){
-     perror("Bind Failed");
-     return 0;
- }
- int lsn = listen(sockfd,5);
- if(lsn<0){
-     perror("Error in listening");
- }
- int pid=0;
- while(1){
- int clilength = sizeof(cli_addr);
- int clisock = accept(sockfd,(struct sockaddr*)&cli_addr,&(clilength));
- if(clisock < 0){
-     printf(" problem in server client socket %s",inet_ntoa(cli_addr.sin_addr));
- }
-pid = fork();
-int recvng;
-if(pid == 0){
-FILE *fp1;
-fp1 = fopen("Recv.mp4","wb");
-while(1){
- recvng= recv(clisock,msg,MAXLINE,0);
-if(recvng <0){
-    perror("Problem in recv");
-}
-else if(recvng == 0)
- { 
-recvLen = fwrite(msg,sizeof(char),recvng,fp1);
-break;
-}
+    struct sockaddr_in serv_addr,cli_addr;
+    if((sockfd = socket(PF_INET,SOCK_STREAM,0))<0){
+        perror("Server socket not created");
+    } 
+    memset (&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_addr.s_addr =  htonl(+);
 
-recvLen = fwrite(msg,sizeof(char),recvng,fp1);
-}    
-}
+    if(bind(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr))<0){
+        perror("Bind Failed");
+        return 0;
+    }
+    int lsn = listen(sockfd,5);
+    
+    if(lsn<0){
+        perror("Error in listening");
+    }
 
-else
-  {
-   close(newsockfd);//sock is closed BY PARENT
-  }
+    int pid=0;
+    while(1){
+        int clilength = sizeof(cli_addr);
+        int clisock = accept(sockfd,(struct sockaddr*)&cli_addr,&(clilength));
+        if(clisock < 0){
+            printf(" problem in server client socket %s",inet_ntoa(cli_addr.sin_addr));
+        }
+        pid = fork();
+        int recvng;
+        if(pid == 0){
+            fp1 = fopen("Recv.mp4","wb");
+            while(1){
+                recvng= recv(clisock,msg,MAXLINE,0);
+                if(recvng <0){
+                    perror("Problem in recv");
+                }
+                else if(recvng == 0){ 
+                    recvLen = fwrite(msg,sizeof(char),recvng,fp1);
+                    break;
+                }
 
+                recvLen = fwrite(msg,sizeof(char),recvng,fp1);
+            }    
+        }
+        else {
+            close(sockfd);//sock is closed BY PARENT
+        }
 
-
-printf("Msg from client %lu\n",recvLen);
- }
- fclose(fp1);
+        printf("Msg from client %lu\n",recvLen);
+    }
+    
+    fclose(fp1);
+    return 0;
 }
--- socket.c
+++ socket.c
@@ -7,77 +7,76 @@
 #include <arpa/inet.h> 
 #include <netinet/in.h> 
 
-#define PORT	 8081 
+#define PORT	 8086
 #define MAXLINE 1024 
+#define PEER_IP "127.0.0.1"
 
 int main(){
- int sockfd;
- struct  sockaddr_in myaddr;
- size_t newLen;
- //char *my_messsage = "this is a test message";
- /*printf("Enter file location");
- char *loc;
- fgets(loc,64,stdin);
- printf("%s",loc);*/
+    int sockfd;
+    struct  sockaddr_in myaddr;
+    size_t newLen;
+
     char *source = NULL;
-FILE *fp = fopen("vibhor.mp4", "rb");
-if (fp != NULL) {
-    /* Go to the end of the file. */
-    if (fseek(fp, 0L, SEEK_END) == 0) {
-        /* Get the size of the file. */
-        long bufsize = ftell(fp);
-        if (bufsize == -1) { /* Error */ }
+    FILE *fp = fopen("vibhor.mp4", "rb");
+    if (fp != NULL) {
+        /* Go to the end of the file. */
+        if (fseek(fp, 0L, SEEK_END) == 0) {
+            /* Get the size of the file. */
+            long bufsize = ftell(fp);
 
-        /* Allocate our buffer to that size. */
-        source = malloc(sizeof(char) * (bufsize + 1));
+            if (bufsize == -1) { /* Error */ }
 
-        /* Go back to the start of the file. */
-        if (fseek(fp, 0L, SEEK_SET) != 0) { /* Error */ }
+            /* Allocate our buffer to that size. */
+            source = malloc(sizeof(char) * (bufsize + 1));
 
-        /* Read the entire file into memory. */
-         newLen = fread(source, sizeof(char), bufsize, fp);
-        if ( ferror( fp ) != 0 ) {
-            fputs("Error reading file", stderr);
-        } else {
-            source[newLen++] = '\0'; /* Just to be safe. */
+            /* Go back to the start of the file. */
+            if (fseek(fp, 0L, SEEK_SET) != 0) { /* Error */ }
+
+            /* Read the entire file into memory. */
+            newLen = fread(source, sizeof(char), bufsize, fp);
+            
+            if ( ferror( fp ) != 0 ) {
+                fputs("Error reading file", stderr);
+            } else {
+                source[newLen++] = '\0'; /* Just to be safe. */
+            }
         }
+        
+        printf("%lu",newLen);
+        fclose(fp);
     }
-    printf("%lu",newLen);
 
-    fclose(fp);
-}
+    if((sockfd = socket(PF_INET,SOCK_STREAM,0))<0){
+        perror("Cannot create socket");
+        return 0;
+    }
+    memset(&myaddr,0,sizeof(myaddr));
 
- if((sockfd = socket(PF_INET,SOCK_STREAM,0))<0){
-     perror("Cannot create socket");
-     return 0;
- }
- memset(&myaddr,0,sizeof(myaddr));
-// memset((char*)&servaddr, 0, sizeof(servaddr));
+    myaddr.sin_family = AF_INET;
+    myaddr.sin_port = htons(PORT);
+    myaddr.sin_addr.s_addr = inet_addr(PEER_IP);
 
- myaddr.sin_family = AF_INET;
- myaddr.sin_port = htons(PORT);
- myaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    if(connect(sockfd,(struct sockaddr*)&myaddr,sizeof(myaddr))<0){
+        perror("connect Failed");
+        return 0;
+    }
 
- if(connect(sockfd,(struct sockaddr*)&myaddr,sizeof(myaddr))<0){
-     perror("connect Failed");
-     return 0;
- }
-int i=0,j=0,offset = 40000;
-int no = newLen/offset;
-int remain = newLen%offset;
-while(i<no){
+    int i=0,j=0,offset = 40000;
+    int no = newLen/offset;
+    int remain = newLen%offset;
+    while(i<no){
 
-if(send(sockfd,(source+j),offset,0)<0){
-    perror("Send to failed");
-    return 0;
-}
-j+=offset;
-i++;
-}
-if(send(sockfd,source+j,remain,0)<0){
-    perror("Send to failed");
-    return 0;
-}
-close(sockfd);
+        if(send(sockfd,(source+j),offset,0)<0){
+            perror("Send to failed");
+            return 0;
+        }
+        j+=offset;
+        i++;
+    }
+    if(send(sockfd,source+j,remain,0)<0){
+        perror("Send to failed");
+        return 0;
+    }
+    close(sockfd);
     return 0;
 }
