ef30274bebd5b0de85245163a41960daa433ec7b
Jai Singhal <jaisinghal48@gmail.com>
2019-10-18 17:33:31 +0530

    Update client and server

--- client.cpp
+++ client.cpp
@@ -9,9 +9,8 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <errno.h>
-#include <regex> 
 
-#define PORT 8085
+#define PORT 8069
 #define BUFFERSIZE 99999
 #define OFFSET 40000
 #define PEER_IP "127.0.0.1"
@@ -19,11 +18,10 @@
 using namespace std;
 
 int main(){
-
-
     int sockfd;
     struct  sockaddr_in myaddr;
-    size_t newLen;
+    size_t recvLen;
+
     char BUFFER[BUFFERSIZE];
 
     if((sockfd = socket(PF_INET,SOCK_STREAM,0))<0){
@@ -41,7 +39,6 @@ int main(){
     myaddr.sin_port = htons(PORT);
     myaddr.sin_addr.s_addr = inet_addr(ip.c_str());
 
-
     cout << "Please enter your IP address ";
     cin >> ip;
 
@@ -50,12 +47,6 @@ int main(){
         perror("connect Failed");
         return 0;
     }
-
-    ofstream outfile("peers.txt");
-    outfile << ip; 
-    outfile.close();
-
-
 
     recv(sockfd, BUFFER, BUFFERSIZE, 0);  //directory structure
     cout << BUFFER << endl;
@@ -83,47 +74,23 @@ int main(){
     // }
     cout << fileName << endl;
     
-    char *source = NULL;
-    FILE *fp = fopen(fileName.c_str(), "rb");
-    if (fp != NULL) {
-        /* Go to the end of the file. */
-        if (fseek(fp, 0L, SEEK_END) == 0) {
-            /* Get the size of the file. */
-            long bufsize = ftell(fp);
-            if (bufsize == -1) { /* Error */ }
-            /* Allocate our buffer to that size. */
-            source = new char[bufsize+1];
-            /* Go back to the start of the file. */
-            if (fseek(fp, 0L, SEEK_SET) != 0) { /* Error */ }
-            /* Read the entire file into memory. */
-            newLen = fread(source, sizeof(char), bufsize, fp);
-            if ( ferror( fp ) != 0 ) {
-                fputs("Error reading file", stderr);
-            } else {
-                source[newLen++] = '\0'; /* Just to be safe. */
-            }
+    int totalSize = 0, received;
+    FILE *fp = fopen(fileName.c_str(), "wb");
+    while(1){
+        received= recv(sockfd,BUFFER,BUFFERSIZE,0);
+        totalSize += received;
+        if(received < 0){
+            perror("Problem in recv");
+            break;
         }
-        printf("%lu",newLen);
-        fclose(fp);
-    }
-
-
-
-    int i=0,j=0,offset = OFFSET;
-    int no = newLen/offset;
-    int remain = newLen%offset;
-    while(i<no){
-        if(send(sockfd,(source+j),offset,0)<0){
-            perror("Send to failed");
-            return 0;
+        else if(received == 0){ 
+            recvLen = fwrite(BUFFER,sizeof(char),received,fp);
+            break;
         }
-        j+=offset;
-        i++;
+        recvLen = fwrite(BUFFER,sizeof(char),received,fp);
     }
-    if(send(sockfd,source+j,remain,0)<0){
-        perror("Send to failed");
-        return 0;
-    }
+    cout << "Total size" << totalSize << endl;  
+
     close(sockfd);
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -16,9 +16,9 @@
 
 using namespace std;
 
-#define PORT 8085
+#define PORT 8069
 #define BUFFERSIZE 99999 
-
+#define OFFSET 40000
 
 string handleNewPeer(char* ipAddr){
     string line;
@@ -52,10 +52,17 @@ string handleNewPeer(char* ipAddr){
 }
 
 string getSharedDir(){
-    string mySharedDir;
+    string mySharedDir = "";
     DIR *dp;
     cout << "Please enter your shared directory path: ";
     cin >> mySharedDir;
+
+    while(mySharedDir.length() == 0){
+        cout << "You have not entered the path" << endl;
+        cout << "Please enter your shared directory path: ";
+        cin >> mySharedDir;
+    }
+
     dp = opendir(mySharedDir.c_str());
     if (dp == NULL) {
         cout << "Error " << errno << " while trying opening" << mySharedDir << endl;
@@ -64,111 +71,137 @@ string getSharedDir(){
     return mySharedDir;
 }
 
+void dieError(const char* msg){
+    perror(msg);
+    exit(-1);
+}
 
 
 int main(){
-    size_t recvLen,newLen = 46258;
-    char BUFFER[BUFFERSIZE];
+    size_t newLen = 46258, recvLen;
+    char* BUFFER;
     int sockfd;
     FILE *fp1;
 
     struct sockaddr_in serv_addr,cli_addr;
     if((sockfd = socket(PF_INET,SOCK_STREAM,0))<0){
-        perror("Server socket not created");
+        dieError("Server socket not created");
     } 
     memset (&serv_addr,0,sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-    // serv_addr.sin_addr.s_addr =  htonl(0);
     serv_addr.sin_addr.s_addr =  htonl(INADDR_ANY);
 
     if(bind(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr))<0){
-        perror("Bind Failed");
-        return 0;
+        dieError("Bind Failed");
     }
     int lsn = listen(sockfd,5);
-    
     if(lsn<0){
-        perror("Error in listening");
+        dieError("Error in listening");
     }
 
     int pid=0;
+    string mySharedDir = "";
+
     while(1){
         socklen_t clilength = sizeof(cli_addr);
         int clisock = accept(sockfd,(struct sockaddr*)&cli_addr,&(clilength));
+
         if(clisock < 0){
             printf(" problem in server client socket %s",inet_ntoa(cli_addr.sin_addr));
         }
-        int recvng;
 
         if ((pid = fork()) == -1){
             close(clisock);
             continue;
         }
         else if(pid == 0){ //child
-            // recv(clisock, buffer, BUFFERSIZE, 0);  //ip address
 
-            string mySharedDir = getSharedDir();
-            vector<string> ignore = buildIgnoreVector();
-            printDirectoryStructure(mySharedDir, "│", ignore);
-            const char* dirStr = directoryStructure.c_str();
+            // directoryStructure == global var
+            if(mySharedDir.length() == 0){
+                mySharedDir = getSharedDir();
+                vector<string> ignore = buildIgnoreVector();
+                printDirectoryStructure(mySharedDir, "│", ignore);
+            }
 
-            if( send(clisock,directoryStructure.c_str(),directoryStructure.length(), 0) < 0 ){
+            if(send(clisock,directoryStructure.c_str(),directoryStructure.length(), 0) < 0){
                 perror("Send to failed");
                 return 0;
             }
-            
-            recv(sockfd, BUFFER, BUFFERSIZE, 0);  
-            
-            DIR *dp;
+
+            // recieve the file path     
+            BUFFER = new char[1000];       
+            recv(clisock, BUFFER, 1000, 0);  
+
+            // DIR *dp;
             string sBuffer(BUFFER);
-            string fullPath = mySharedDir + sBuffer; 
+            string fullPath = mySharedDir + '/' + sBuffer; 
+            delete BUFFER;
 
-            /* TODO
-                CHECK FOR PROPER PATH
-            */ 
-            
-            dp = opendir(fullPath.c_str());
-            if (dp == NULL) {
-                cout << "Error " << errno << " while trying opening" << fullPath << endl;
-                exit(-1);
-            }
+
             cout << "Path entered was: " << fullPath << endl;
+            char *source = NULL;
+            size_t newLen;
+            FILE *fp = fopen(fullPath.c_str(), "rb");
 
-
-            // CHECK THE PATH
-
-            fp1 = fopen(fullPath.c_str(), "wb");
-            int totalSize = 0;
-            while(1){
-                recvng= recv(clisock,BUFFER,BUFFERSIZE,0);
-                totalSize += recvng;
-                if(recvng < 0){
-                    perror("Problem in recv");
-                    break;
+            if (fp < 0) {
+                /* failure */
+                if (errno == EEXIST) {
+                    cout << "Error " << errno << " while trying accessing " << fullPath << endl;
+                    return 0;                
                 }
-                else if(recvng == 0){ 
-                    recvLen = fwrite(BUFFER,sizeof(char),recvng,fp1);
-                    break;
-                }
-                recvLen = fwrite(BUFFER,sizeof(char),recvng,fp1);
             }
-            cout << "Total size" << totalSize << endl;  
-            close(clisock);
+            else {
+                /* Go to the end of the file. */
+                if (fseek(fp, 0L, SEEK_END) == 0) {
+                    /* Get the size of the file. */
+                    long bufsize = ftell(fp);
+                    if (bufsize == -1) { /* Error */ }
+                    /* Allocate our buffer to that size. */
+                    source = new char[bufsize+1];
+                    /* Go back to the start of the file. */
+                    if (fseek(fp, 0L, SEEK_SET) != 0) { /* Error */ }
+                    /* Read the entire file into memory. */
+                    newLen = fread(source, sizeof(char), bufsize, fp);
+                    if ( ferror( fp ) != 0 ) {
+                        fputs("Error reading file", stderr);
+                    } else {
+                        source[newLen++] = '\0'; /* Just to be safe. */
+                    }
+                }
+                printf("Total length of the file: %lu",newLen);
+                fclose(fp);
+            }
+
+            int i=0,j=0,offset = OFFSET;
+            int no = newLen/offset;
+            int remain = newLen%offset;
+            while(i<no){
+                if(send(clisock,(source+j),offset,0)<0){
+                    perror("Send to failed");
+                    return 0;
+                }
+                j+=offset;
+                i++;
+            }
+            if(send(clisock,source+j,remain,0)<0){
+                perror("Send to failed");
+                return 0;
+            }
+
+            close(sockfd);
             close(lsn);
         }
 
         else if(pid > 0) { //parent
-            // recieve a ip from new client
-
-
+            // receive a ip from new client
             close(clisock);
             // counter++;
             continue;
         }
 
         printf("BUFFER from client %lu\n",recvLen);
-    }
+    } // end of while
     
     fclose(fp1);
     return 0;
